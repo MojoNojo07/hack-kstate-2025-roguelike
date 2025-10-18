decec0c6c77562b2880f71ec374a2a617eb30650
AraReighard <ara.reighard@icloud.com>
2025-10-17 23:14:37 -0700

    Added hp and defense to tiles

--- Tile.java
+++ Tile.java
@@ -1,11 +1,19 @@
 public class Tile {
     private final char character;
 
+    private int maxHp;
+    private int defense;
+    private int hp;
+
     /**
      * Represents a tile on the map
      * @param character the single character that will show on the board
+     * @param hp the hitpoints the tile has before it gets destroyed
+     * @param defense subtracts from damage dealt to hp
      */
-    public Tile(char character) {
+    public Tile(char character, int hp, int defense) {
         this.character = character;
+        this.maxHp = hp;
+        this.defense = defense;
     }
 }
