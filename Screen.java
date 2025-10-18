cd8fbada28b75fed666b91b7bbd7731fb3170cd8
MojoNojo06 <azaleah.j06@gmail.com>
2025-10-18 00:33:42 -0500

    window & text

---
+++ Screen.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+
+public class Screen extends JFrame implements KeyListener {
+
+    private JLabel mainGameArea;
+
+    Screen(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.addKeyListener(this);
+        this.setVisible(true);
+        this.setTitle("Kill the NecromanCEO");
+
+        this.getContentPane().setBackground(Color.BLACK);
+
+
+        mainGameArea = new JLabel("true!!", SwingConstants.CENTER);
+        mainGameArea.setSize(500, 500);
+        mainGameArea.setForeground(Color.WHITE);
+        mainGameArea.setFont(new Font("Bebas Neue", Font.PLAIN, 24));
+        this.add(mainGameArea);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        String message = "<html>key " + e.getKeyCode() + " pressed <br/>You sure did press that button!! </html>";
+        mainGameArea.setText(message);
+        System.out.println(message);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {  
+        System.out.println("key " + e.getKeyCode() + " released");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("key " + e.getKeyCode() + " typed");
+    }
+    
+}
