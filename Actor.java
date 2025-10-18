6691cbe41ab17942a8d7c30a4d9411eb334a6641
AraReighard <ara.reighard@icloud.com>
2025-10-18 11:38:44 -0700

    added move method to actor class

--- Actor.java
+++ Actor.java
@@ -3,9 +3,22 @@ public class Actor extends Tile {
         super(character, color, hp, defense, x, y);
     }
 
+    /**
+     * Attempts to move the player character in the direction inputted. Stops from collision.
+     * @param x how much to move in the x direction
+     * @param y how much to move in the y direction
+     */
     private void move(int x, int y) {
         for (int i = Math.abs(x); i > 0; i--) {
-            if ()
+            if (Main.currentFloor.getTile(x + x / Math.abs(x), 0) == null) {
+                Main.currentFloor.moveTile(x, y, x + x / Math.abs(x), 0);
+            }
+        }
+
+        for (int i = Math.abs(y); i > 0; i--) {
+            if (Main.currentFloor.getTile(0, y + y / Math.abs(y)) == null) {
+                Main.currentFloor.moveTile(x, y, 0, y + y / Math.abs(y));
+            }
         }
     }
 }
