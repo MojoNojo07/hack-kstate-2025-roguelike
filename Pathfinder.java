103c8c7fbee82389a9805b729c2db9fa122a2d55
AraReighard <ara.reighard@icloud.com>
2025-10-18 14:28:46 -0700

    changed pathfinder to reflect prev changes

--- Pathfinder.java
+++ Pathfinder.java
@@ -1,22 +1,22 @@
 public class Pathfinder {
     /**
-     * Shows the closest direction to take to get from one tile to another.
-     * @param tile the tile to go from
-     * @param otherTile the tile to go to
+     * Shows the closest direction to take to get from one actor to another.
+     * @param actor the actor to go from
+     * @param otherActor the actor to go to
      * @return an array of the x and y of where to go. Values of -1, 0, or 1.
      */
 
     // Checks which of the two values (x vs y) are greater, then sets that direction to 1 or -1.
-    public final static int[] findPath(Tile tile, Tile otherTile) {
+    public final static int[] findPath(Actor actor, Actor otherActor) {
 
         int x = 0;
         int y = 0;
 
-        if (otherTile.getX() - tile.getX() > otherTile.getY() - tile.getY()) {
-            x = (otherTile.getX() - tile.getX()) / Math.abs(otherTile.getX() - tile.getX());
+        if (otherActor.getX() - actor.getX() > otherActor.getY() - actor.getY()) {
+            x = (otherActor.getX() - actor.getX()) / Math.abs(otherActor.getX() - actor.getX());
         }
         else {
-            y = (otherTile.getY() - tile.getY()) / Math.abs(otherTile.getY() - tile.getY());
+            y = (otherActor.getY() - actor.getY()) / Math.abs(otherActor.getY() - actor.getY());
         }
 
         int[] coordinates = {x, y};
