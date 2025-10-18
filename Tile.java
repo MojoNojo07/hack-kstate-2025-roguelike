302c93a785770f642ea960c052d6e82d3236945d
AraReighard <ara.reighard@icloud.com>
2025-10-17 23:03:36 -0700

    javadoc

--- Tile.java
+++ Tile.java
@@ -1,6 +1,10 @@
 public class Tile {
     private final char character;
 
+    /**
+     * Represents a tile on the map
+     * @param character the single character that will show on the board
+     */
     public Tile(char character) {
         this.character = character;
     }
