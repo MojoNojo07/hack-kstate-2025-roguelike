247f63b8d63f31ce54d904e4870e3378a6d54d0e
MojoNojo06 <azaleah.j06@gmail.com>
2025-10-18 10:19:31 -0500

    moving character & screen updating

--- Screen.java
+++ Screen.java
@@ -11,6 +11,9 @@ import javax.swing.SwingConstants;
 public class Screen extends JFrame implements KeyListener {
 
     private JLabel mainGameArea;
+    private String board;
+    private int characterX;
+    private int characterY;
 
     Screen(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,17 +26,48 @@ public class Screen extends JFrame implements KeyListener {
         this.getContentPane().setBackground(Color.BLACK);
 
 
-        mainGameArea = new JLabel("true!!", SwingConstants.CENTER);
+        characterX = 2;
+        characterY = 3;
+
+        board = "";
+
+        mainGameArea = new JLabel(board, SwingConstants.CENTER);
         mainGameArea.setSize(500, 500);
         mainGameArea.setForeground(Color.WHITE);
         mainGameArea.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 24));
         this.add(mainGameArea);
     }
 
+    private void updateBoard() {
+        board = "";
+        int size = 5;
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                if (j == characterX && i == characterY) {
+                    board += "G";
+                } else {
+                    board += ".";
+                }
+            }
+            board += "<br>";
+        }
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode == 87) {
+            characterY--;
+        } else if (keyCode == 68) {
+            characterX++;
+        } else if (keyCode == 83) {
+            characterY++;
+        } else if (keyCode == 65) {
+            characterX--;
+        }
         String message = "key " + e.getKeyCode() + " pressed";
-        mainGameArea.setText("<html>" + message + "<br/>You sure did press that button!! </html>");
+        this.updateBoard();
+        mainGameArea.setText("<html>" + board + "</html>");
         System.out.println(message);
     }
 
