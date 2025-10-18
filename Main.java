5d5f314f8b555db32bb0d3b47868e66bb9ffbfbf
MojoNojo06 <azaleah.j06@gmail.com>
2025-10-18 13:55:46 -0500

    the screen is just an input handler now, combined

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main {
     public static final GridMap currentFloor = new GridMap(0, 0);
 
     public static void main(String args[]) {
-        new Screen();
+        new InputHandler();
     }
 }
