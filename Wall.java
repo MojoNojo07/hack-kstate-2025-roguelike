f568a3b1bb05398fd03fd1fdf6048457b5c505e4
AraReighard <ara.reighard@icloud.com>
2025-10-18 14:27:48 -0700

    removed x and y from wall tiles

--- Wall.java
+++ Wall.java
@@ -3,8 +3,8 @@ public class Wall extends Tile {
 
     private char defaultCharacter;
 
-    public Wall(char character, String color, int hp, int defense, int x, int y, char defaultCharacter) {
-        super(character, color, hp, defense, x, y);
+    public Wall(char character, String color, int hp, int defense, char defaultCharacter) {
+        super(character, color, hp, defense);
         this.defaultCharacter = defaultCharacter;
     }
 
