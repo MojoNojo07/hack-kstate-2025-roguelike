267b3f132772addd6bad288c22e9508e684ced75
AraReighard <ara.reighard@icloud.com>
2025-10-17 23:03:45 -0700

    added gridmap

--- GridMap.java
+++ GridMap.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+/**
+ * A grid for the level map
+ * @param n the rows
+ * @param m the columns
+ */
 public class GridMap {
-    
+    public GridMap(int n, int m) {
+        Tile[][] grid = new Tile[n][m];
+    }
 }
