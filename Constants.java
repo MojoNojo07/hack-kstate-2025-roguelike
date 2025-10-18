3fc688712235bd624842e566f1455cfeddff65a6
AraReighard <ara.reighard@icloud.com>
2025-10-18 00:16:17 -0700

    Basic getMapUI() implemented

--- Constants.java
+++ Constants.java
@@ -1,3 +1,9 @@
 public final class Constants {
+
     public static final int CHUNK_SIZE = 20;
+    
+    // Need to be divisible by 2
+    public static final int MAP_VIEW_X = 36;
+    public static final int MAP_VIEW_Y = 20;
+
 }
--- GridMap.java
+++ GridMap.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.lang.Math;
 
 /**
  * A grid for the level map
@@ -6,7 +7,35 @@ import java.util.Arrays;
  * @param m the columns
  */
 public class GridMap {
+    private Tile[][][][] grid;
+    private int mapXMax;
+    private int mapYMax;
+
     public GridMap(int n, int m) {
-        Tile[][] grid = new Tile[n][m];
+        this.grid = new Tile[n][m][Constants.CHUNK_SIZE][Constants.CHUNK_SIZE];
+        this.mapXMax = n * Constants.CHUNK_SIZE;
+        this.mapYMax = m * Constants.CHUNK_SIZE;
+        //TODO: add tile generation and such
     }
+
+    public String[][] getMapUI() {
+
+        String[][] mapUI = new String[Constants.MAP_VIEW_X][Constants.MAP_VIEW_Y];
+
+        //TODO CHANGE THISSSSSSSSSSSSSSSSS
+        int playerX = 0;
+        int playerY = 0;
+        // gets the top left corner of the viewport, making sure to clamp it to avoid OOB errors
+        int startX = Math.min(playerX - Constants.MAP_VIEW_X / 2, mapXMax - Constants.CHUNK_SIZE - 1);
+        int startY = Math.min(playerY - Constants.MAP_VIEW_Y / 2, mapYMax - Constants.CHUNK_SIZE - 1);
+
+        for (int x = 0; x < Constants.MAP_VIEW_X; x++) {
+            for (int y = 0; y < Constants.MAP_VIEW_Y; y++) {
+                //pass for now
+            }
+        }
+
+        return null;
+    }
+
 }
