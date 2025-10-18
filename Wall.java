8d116dfb8e120bf37335687d50abdb7f0e2b5b64
AraReighard <ara.reighard@icloud.com>
2025-10-18 01:21:30 -0700

    added method to update wall to a default character

--- Wall.java
+++ Wall.java
@@ -1,7 +1,17 @@
 public class Wall extends Tile {
 
-    public Wall(char character, int hp, int defense) {
-        super(character, hp, defense);
+    private char defaultCharacter;
+
+    public Wall(char character, String color, int hp, int defense, int x, int y, char defaultCharacter) {
+        super(character, color, hp, defense, x, y);
+        this.defaultCharacter = defaultCharacter;
+    }
+
+    /**
+     * Updates the wall to show as the default wall character
+     */
+    public void showWall() {
+        this.setCharacter(this.defaultCharacter);
     }
     
 }
