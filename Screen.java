6b3e2a95853fad575afdeeddc99d8638dd8c4185
MojoNojo06 <azaleah.j06@gmail.com>
2025-10-18 10:28:38 -0500

    separated screen and inputs (janky)

--- Screen.java
+++ Screen.java
@@ -1,29 +1,30 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 
-public class Screen extends JFrame implements KeyListener {
+public class Screen extends JFrame {
 
-    private JLabel mainGameArea;
-    private String board;
-    private int characterX;
-    private int characterY;
+    public JLabel mainGameArea;
+    public String board;
+    public int characterX;
+    public int characterY;
+    InputHandler inputHandler;
 
     Screen(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setLayout(null);
-        this.addKeyListener(this);
         this.setVisible(true);
         this.setTitle("Kill the NecromanCEO");
 
         this.getContentPane().setBackground(Color.BLACK);
+
+        inputHandler = new InputHandler(this);
+        this.addKeyListener(inputHandler);
 
 
         characterX = 2;
@@ -38,7 +39,7 @@ public class Screen extends JFrame implements KeyListener {
         this.add(mainGameArea);
     }
 
-    private void updateBoard() {
+    public void updateBoard() {
         board = "";
         int size = 5;
         for(int i = 0; i < size; i++) {
@@ -51,34 +52,6 @@ public class Screen extends JFrame implements KeyListener {
             }
             board += "<br>";
         }
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == 87) {
-            characterY--;
-        } else if (keyCode == 68) {
-            characterX++;
-        } else if (keyCode == 83) {
-            characterY++;
-        } else if (keyCode == 65) {
-            characterX--;
-        }
-        String message = "key " + e.getKeyCode() + " pressed";
-        this.updateBoard();
-        mainGameArea.setText("<html>" + board + "</html>");
-        System.out.println(message);
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {  
-        // nothin 
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // nothin
     }
     
 }
