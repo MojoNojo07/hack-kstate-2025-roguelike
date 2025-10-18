73e066be375c58093ae8ab67a6d3ee5cec6fa4b2
AraReighard <ara.reighard@icloud.com>
2025-10-18 13:34:08 -0700

    added a pathfinding tool

---
+++ Pathfinder.java
@@ -0,0 +1,23 @@
+public class Pathfinder {
+    /**
+     * Shows the closest direction to take to get from one tile to another.
+     * @param tile the tile to go from
+     * @param otherTile the tile to go to
+     * @return an array of the x and y of where to go. Values of -1, 0, or 1.
+     */
+    public final static int[] findPath(Tile tile, Tile otherTile) {
+
+        int x = 0;
+        int y = 0;
+
+        if (otherTile.getX() - tile.getX() > otherTile.getY() - tile.getY()) {
+            x = (otherTile.getX() - tile.getX()) / Math.abs(otherTile.getX() - tile.getX());
+        }
+        else {
+            y = (otherTile.getY() - tile.getY()) / Math.abs(otherTile.getY() - tile.getY());
+        }
+
+        int[] coordinates = {x, y};
+        return coordinates;
+    }
+}
