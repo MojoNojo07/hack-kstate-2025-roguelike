f07e0b3347ef9e21ed6cbee4dbab33817028c379
AraReighard <ara.reighard@icloud.com>
2025-10-18 23:23:02 -0700

    removed junk test objects

--- Main.java
+++ Main.java
@@ -6,13 +6,7 @@ class Main {
 
     public static void main(String args[]) {
         currentFloor.addActor(player, 2, 2);
-
-        Tile wall = new Tile('X', "\u001B[40m", 10, 20, 10, 10);
-
-        currentFloor.setTile(wall, wall.getX(), wall.getY());
-        currentFloor.setTile(wall, wall.getX()+1, wall.getY());
-        currentFloor.setTile(wall, wall.getX(), wall.getY()+1);
-        currentFloor.setTile(wall, wall.getX()+1, wall.getY()+1);
+        
         new InputHandler(player);
     }
 }
