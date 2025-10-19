ebb2621f6b4374e0d6e926dfcdcf46fb32f4f6e2
AraReighard <ara.reighard@icloud.com>
2025-10-19 00:46:54 -0700

    created lootbox.java

---
+++ LootBox.java
@@ -0,0 +1,5 @@
+public class LootBox extends Tile {
+
+    public LootBox(char character, String color, int hp, int defense, int x, int y) {
+        super(character, color, hp, defense, x, y);
+    }
